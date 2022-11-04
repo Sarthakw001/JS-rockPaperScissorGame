1adc90aab8e3818ecd53922eb8cc100fc063fb01
Sarthakw001 <sarthakw01@gmail.com>
2022-11-04 11:13:07 +0530

    confeti added

--- js/main.js
+++ js/main.js
@@ -66,6 +66,7 @@ function clickEvent() {
 
       if (winner == " User") {
         userScore++;
+        celebrate();
         document.getElementById("userScoreVal").innerText = userScore;
       } else if (winner == " Computer") {
         pcScore++;
@@ -80,3 +81,40 @@ function clickEvent() {
   });
 }
 clickEvent();
+function celebrate() {
+  var count = 200;
+  var defaults = {
+    origin: { y: 0.7 },
+  };
+
+  function fire(particleRatio, opts) {
+    confetti(
+      Object.assign({}, defaults, opts, {
+        particleCount: Math.floor(count * particleRatio),
+      })
+    );
+  }
+
+  fire(0.25, {
+    spread: 26,
+    startVelocity: 55,
+  });
+  fire(0.2, {
+    spread: 60,
+  });
+  fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8,
+  });
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2,
+  });
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 45,
+  });
+}
